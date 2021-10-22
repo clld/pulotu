411f0f48c02f3dd46c87d520ceb2cd034b6f2ea6
Robert Forkel <xrotwang@googlemail.com>
2021-10-22 08:08:54 +0200

    fix tests

--- pulotu/tests/test_functional.py
+++ pulotu/tests/test_functional.py
@@ -1,2 +1,3 @@
 def test_home(app):
     app.get_html('/', status=200)
+    app.get_html('/about', status=200)
--- pulotu/tests/test_selenium.py
+++ pulotu/tests/test_selenium.py
@@ -5,7 +5,7 @@ def test_ui(selenium):
     selenium.browser.get(selenium.url('/'))
 
     time.sleep(1)
-    selenium.browser.get(selenium.url('/languages'))
+    selenium.browser.get(selenium.url('/culture/'))
     time.sleep(3)
     selenium.browser.get(selenium.url('/sources'))
 
